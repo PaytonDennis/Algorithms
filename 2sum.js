1cae06bd3687d14474b626380625f58a976a2d74
PaytonDennis <102490243+PaytonDennis@users.noreply.github.com>
2026-01-08 19:07:32 -0700

    practice

--- 2sum.js
+++ 2sum.js
@@ -10,7 +10,7 @@ console.log(twoSum([3, 2, 4], 6));       // [1, 2]
 console.log(twoSum([3, 3], 6));          // [0, 1]
 
 
-
+// O(n^2)
 function twoSum(nums, target){
     for (let i = 0; i < nums.length; i++)
         for (let j = i + 1; j < nums.length; j++)
@@ -18,11 +18,95 @@ function twoSum(nums, target){
                 return [i,j]     
 }
 
-
-
 function twoSum(nums, target){
     for (let i = 0; i < nums.length; i++)
         for (let j = i + 1;  j < nums.length; j++)
             if (nums[i] + nums[j] == target)
                 return [i,j]
+}
+
+function twoSum(nums, target){
+    for (i = 0; i < nums.length; i++)
+        for (j = i+1; j < nums.length; j++)
+    if (nums[i] + nums[j] == target)
+        return [i,j]
+}
+
+// Hash map solution - O(n)
+function twoSumHashMap(nums, target) {
+    let map = new Map();
+    
+    for (let i = 0; i < nums.length; i++) {
+        // Calculate what number we need
+        let complement = target - nums[i];
+        
+        // Check if we've seen it
+        if (map.has(complement)) {
+            return [map.get(complement), i];
+        }
+        
+        // Store this number and its index
+        map.set(nums[i], i);
+    }
+}
+
+
+// Find two numbers in array that sum to target (brute force)
+function twoSum(nums, target) {
+    for (let i = 0; i < nums.length; i++)
+        for (let j = i + 1; j < nums.length; j++)
+            if (nums[j] + nums [j])
+                return [i,j]
+}
+
+
+
+function twoSumMemory(){
+let nums = [0,2,3]
+let target = 4
+    let map = new Map()
+    for (let i = 0; i < nums.length; i++) {
+        let complement = target - nums[i];
+        if (map.has(complement)) {
+            return [map.get(complement), i];
+        }
+        
+        map.set(nums[i], i);
+    }
+}
+
+// hashmap
+function twoSum(nums, target) {
+    let map = new Map()
+    for (let i = 0; i < nums.length; i++){
+            let complement = target - nums[i]
+        if (map.has(complement)) {
+            return [map.get(complement),i]
+        }
+        map.set(nums[i],i)
+    }
+}
+
+// [1,2,3] target = 5
+function twoSumHash(nums, target){
+    let map = new Map()
+    for(let i = 0; i < nums.length; i++ ){
+         let complement = target - nums[i]
+         if (map.has(complement)){
+            return [map.get(complement),i]
+         }
+    }
+    map.set[nums[i], i]
+}
+
+
+function twoSum(nums, target){
+    let map = new Map()
+    for (let i = 0; i < nums.length; i++){
+        let complement = target - nums[i]
+        if(map.has(complement)){
+            return([map.get(complement),i])
+        }
+        map.set(nums[i], i)
+    }
 }
--- containsDuplicate.js
+++ containsDuplicate.js
@@ -45,15 +45,6 @@ console.log(containsDuplicate([1, 2, 3, 4]));  // false
 console.log(containsDuplicate([1, 1, 1, 1]));  // true
 
 
-var containsDuplicateSet = function (nums){
-    let seen = new Set();
-    for (let i = 0; i < nums.length; i++){
-        if (seen.has(nums[i]) {
-            
-        })
-    }
-}
-
 
 // function containsDuplicate(nums){
 //     for (let i = 0; i < nums.length; i++)
@@ -62,3 +53,14 @@ var containsDuplicateSet = function (nums){
 //             return true
 //     return false;
 // }
+
+
+function containsDuplicate(nums){
+    for (let i = 0; i < nums.length; i++)
+        for (let j = i + 1; j < nums.length; j ++)
+        if(nums[i] == nums[j])
+            return true
+    return false;
+}
+
+// hashmap method
