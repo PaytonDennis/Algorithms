452b687318832261343e84cb1737c07bedc745fc
PaytonDennis <102490243+PaytonDennis@users.noreply.github.com>
2026-01-10 17:36:32 -0700

    Update isAnagram.js

--- isAnagram.js
+++ isAnagram.js
@@ -13,10 +13,21 @@
 
 
    function isAnagram(s,t){
-    string = s.split("").sort("").join("")
-    string2 = t.split("").sort("").join("")
+    string = s.split("").sort().join("")
+    string2 = t.split("").sort().join("")
     if (string === string2){
         return true
     }
     return false
+   }
+
+
+   function isAnagram(s,t){
+    let string1 = s.split("").sort().join("")
+    let string2 = t.split("").sort().join("")
+
+        if (string1 === string2){
+            return true
+        }
+        return false
    }
